613f6d4cdad71203b532a3701f9135b5a06fa6e4
Leman Kwok <leman@lmswork.com>
2014-01-12 16:57:30 +0800

    Supporting Modal Stacks, Popage allowed to use screen size, call a new instance from iframe, and able to close all screen.

--- popage.js
+++ popage.js
@@ -35,13 +35,15 @@ var defaults = {
 	paddingTop:0,
 	paddingBottom:0,
 	paddingLeft:0,
-	paddingRight:0
-}
+	paddingRight:0,
+	boundToWindow:false,
+	useScrollbar:true
+};
 
 var Popage = function(options){
 	this.options = options;
 	this.initialize.apply(this,arguments);
-}
+};
 Popage.prototype = {
 	options:null,
 	$el: null,
@@ -86,7 +88,9 @@ Popage.prototype = {
 	    	for(var key in options){
 	    		this.options[key] = options[key];
 	    	}
-	    	if(options.href)
+	    	if(options.url)
+		    	href = options.url;
+	    	else if(options.href)
 		    	href = options.href;
 	    	else if(options.html)
 	    		html = options.html;
@@ -105,21 +109,27 @@ Popage.prototype = {
 	    	
 	    	$content.find('.popage-close').on('click',function(){
 	    		self.close();
-	    	})
+	    	});
 	    	
 	    	$wrap.append($content);
 	  		this.$frame.append($wrap);
 	    	this._width = $content.width();
 	    	this._height = $content.height();
 	    }else{
-	    	
-		    this.$frame.append('<iframe class="popage-frame-content" src="'+href+'" scrolling="no" frameborder="0" allowtransparency="true" />');
+	    	var scrollbar = options.useScrollbar ? 'auto':'no';
+		    this.$frame.append('<iframe class="popage-frame-content" src="'+href+'" scrolling="'+scrollbar+'" frameborder="0" allowtransparency="true" />');
 	    }
 	    if(options){
 	    	if(options.width)
 	    		this._width = options.width;
 	    	if(options.height)
 	    		this._height = options.height;
+	    		
+	    	if(options.boundToWindow){
+	    		this._width = $(window).width() - options.paddingLeft - options.paddingRight;
+	    		this._height = $(window).height() - options.paddingTop - options.paddingBottom;
+	    		
+	    	}
     	}
 	    this.render();
 	    if(!this._isOpened){
@@ -128,29 +138,29 @@ Popage.prototype = {
 			
 			jQuery(window).on('resize.popage',function(){
 				self.render();
-			})
+			});
 			
 			this.$frame.on('scroll.popage', function(){
 				if(self.options && self.options.onScroll){
 					self.options.onScroll();
 				}
-			})
+			});
 	    }
 	    this._isOpened = true;
 	    
-	    $body.css('overflow','hidden')
+	    $body.css('overflow','hidden');
 	    if(typeof this.onOpened == 'function')
 		    this.onOpened();
 	},
 	close: function(){
 	    this.$el.hide();
-	    jQuery('body').css('overflow',this._beforeState)
+	    jQuery('body').css('overflow',this._beforeState);
 	    this.$frame.empty();
 	    
 	    this.scrollTop(0);
 	    
 	    if(this._isOpened){
-			jQuery(window).off('resize.popage')
+			jQuery(window).off('resize.popage');
 	    }
 	    if(this._isOpened){
 		    this._isOpened = false;
@@ -188,12 +198,12 @@ Popage.prototype = {
 		this.$frame.find('iframe.popage-frame-content').attr('scrolling',val);
 	},
 	
-	getScrollbar: function(val){
+	getScrollbar: function(){
 		return this.$frame.find('iframe.popage-frame-content').attr('scrolling');
 	},
 	
 	scrollLeft: function(){
-		this._scrollLeft = this.$frame.scrollLeft() 
+		this._scrollLeft = this.$frame.scrollLeft();
 		if(arguments.length > 0){
 			this._scrollLeft = arguments[0];
 			this.$frame.scrollLeft(this._scrollLeft);
@@ -238,7 +248,7 @@ Popage.prototype = {
 	    var csh= s.height * 0.5;
 	    
 	    
-	    $wrap.css('top', $body.scrollTop())
+	    $wrap.css('top', $body.scrollTop());
 	    
 	    var fw = this._width, fh = this._height;
 	    if(this.options.paddingLeft > 0) fw += this.options.paddingLeft;
@@ -256,39 +266,68 @@ Popage.prototype = {
 	    prop.width = fw;
 	    prop.height =fh;
 	    $frame.find('.popage-frame-content').css(prop);
+	},
+	
+	destroy: function(){
+		
+		this.$el.remove();
+		this.$shadow.remove();
+		this.$frame.remove();
+		this.$el = null;
+		this.$shadow = null;
+		this.$frame = null;
+		
+		this.onOpened = null;
+		this.onClosed = null;
 	}
 };
 
-var myPopage = null;
+var popages = [];
 
 var getInstance = function(){
-	if(myPopage)
-		return myPopage;
+	if(popages.length > 0)
+		return popages[ popages.length - 1];
 	if(window.parent != window.self && window.parent.$ && window.parent.$.popage)
 		return window.parent.$.popage.getInstance();
 	if(window.parent != window.self && window.top.$ && window.top.$.popage)
 		return window.top.$.popage.getInstance();
 	
 	return null;
-}
+};
 
 var createInstance = function(){
-	
-	var ins = getInstance();
-	if(!ins){
+	var ins = null;
+	//ins = getInstance();
+	//if(!ins){
 		var _options = $.extend({onScroll: function(){
 				jQuery(window).trigger('popage_scroll');
 			}
-		},defaults) 
-		myPopage = ins = new Popage(_options);
+		},defaults) ;
+		ins = new Popage(_options);
+		popages.push(ins);
 		
-	}
+	//}
 	return ins;
-}
+};
 var eventPrefix = 'popage_';
+
+var closeAllInstance = function(){
+	
+	for(var i = 0; i < popages.length; i ++){
+		
+		var ins = popages[i];
+		if(ins){
+			ins.close();
+			ins.destroy();
+			ins = null;
+		}
+	}
+	popages.length = 0;
+};
 
 var popage = function(method, options){
 	
+	var isParentExist = window.parent != window.self && typeof window.parent.popage == 'function';
 	
 	if( arguments.length == 1 && typeof arguments[0] == 'object'){
 		method = 'open';
@@ -297,20 +336,35 @@ var popage = function(method, options){
 	
 	if( method == 'open'){
 		
+		if(isParentExist)
+			return window.parent.popage.apply(window.parent, arguments);
+		
 		var ins = createInstance();
 		ins.open(options);
 		ins.onOpened = function(){
 			jQuery(window).trigger(eventPrefix+'open', ins);
-		}
+		};
 		ins.onClosed = function(){
 			jQuery(window).trigger(eventPrefix+'close', ins);
-		}
+		};
 	}else if( method == 'close'){
+		
+		if(isParentExist)
+			return window.parent.popage.apply(window.parent, arguments);
 		
 		var ins = getInstance();
 		if(ins){
 			ins.close();
+			ins.destroy();
+			ins = null;
+			popages.length --;
+		}else{
+			if(typeof window.console != 'undefined') console.error('Cannot find the last instance.');
 		}
+		
+	}else if( method == 'closeAll'){
+		
+		closeAllInstance();
 		
 	}else if( method == 'width'){
 		
@@ -392,6 +446,8 @@ var popage = function(method, options){
 		
 	}else if( method == 'destroy'){
 		
+		closeAllInstance();
+		
 		if(myPopover){
 			myPopover.destroy();
 			myPopover = null;
@@ -402,7 +458,7 @@ var popage = function(method, options){
 			if(eventName.length> 0 && eventName.substr(0,eventPrefix.length) != eventPrefix)
 				eventName = eventPrefix + eventName;
 			if(arguments.length > 2 && typeof arguments[2] == 'function')
-				jQuery(window).on( eventName, arguments[2])
+				jQuery(window).on( eventName, arguments[2]);
 		}
 	}else if( method == 'off'){
 		
@@ -411,18 +467,20 @@ var popage = function(method, options){
 			if(eventName.length> 0 && eventName.substr(0,eventPrefix.length) != eventPrefix)
 				eventName = eventPrefix + eventName;
 			if(arguments.length > 2 && typeof arguments[2] == 'function')
-				jQuery(window).off( eventName, arguments[2])
-			jQuery(window).off(eventName)
+				jQuery(window).off( eventName, arguments[2]);
+			jQuery(window).off(eventName);
 		}
+	}else if( method == 'instance'){
+		return popages;
 	}
-}
+};
 
 popage.open = function(opts){
-	return popage('open',opts)
-}
+	return popage('open',opts);
+};
 popage.close = function(){
 	return popage('close');
-}
+};
 
 if(typeof jQuery != 'undefined'){
 	jQuery.popage = function(){
@@ -443,20 +501,20 @@ if(typeof window != 'undefined'){
 		if(typeof val != 'undefined')
 			return val;
 		return window; 
-	}
+	};
 
 	window.popage.open = function(opts){
-		return popage('open',opts)
-	}
+		return popage('open',opts);
+	};
 	window.popage.close = function(){
 		return popage('close');
-	}
+	};
 	// Hack: if we loaded this page inside a popage frame, replace window.close
 	// and tell
 	if(window.parent != window.self && typeof window.parent.popage == 'function'){
 		window.close = function(){
 			window.parent.popage('close');
-		}
+		};
 		
 		var frameSizeUpdateTimer = 0;
 		var tellParentSize = function(){
@@ -466,18 +524,35 @@ if(typeof window != 'undefined'){
 			
 				var w = $elm.width(), h = $elm.height();
 				$elm.attr('data-popage-width',w).attr('data-popage-height',h);
+				if(h < 200) h = 200;
+				if(w < 300) w = 300;
 				window.parent.popage('setSize', w,h );
 			}
 			frameSizeUpdateTimer = setTimeout(tellParentSize,250);
-		}
+		};
 		$(window).on('unload',function(){
 			clearTimeout(frameSizeUpdateTimer);
-		})
+		});
 		$('body').ready( function(){
 			tellParentSize();
+		});
+	}else{
+
+		$(window).on('resize', function(){
+			var ins = getInstance();
+			if(!ins) return;
+			var options = ins.options;
+			if(options.boundToWindow){
+				
+	    		ins.setSize(
+					$(window).width() - options.paddingLeft - options.paddingRight,
+					$(window).height() - options.paddingTop - options.paddingBottom
+				);
+			}
 		});
 	}
 	window.popage.defaults = defaults;
 	window.popage.getInstance = getInstance;
+	
 }
 })();
